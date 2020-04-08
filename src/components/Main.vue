ee0649a891462fa8f792c3e2422798a0cde844df
多岐正行 <tagimasayuki@takimasayukinoMacBook-Pro.local>
2020-04-08 18:21:43 +0900

    20200408

---
+++ src/components/Main.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main">
+    <h1 class="my_name">M.Tagi</h1>
+    <h2 class="my_employer">Sattleconsulting</h2>
+    <img class="my_icon" alt="Masayuki_Tagi" src="@/assets/my_icon.jpg">
+  </div>
+</template>
+
+
+<!-- script -->
+
+<script>
+</script>
+
+
+<!-- CSS -->
+
+
+<style scoped>
+.main{
+  background-color: #87ceeb; 
+  height: 600px;
+  text-align: center;
+}
+.my_name{
+  border-bottom: solid 1px	rgb(255, 255, 255);
+  color: rgb(255, 255, 255);
+  font-size: 60px;
+  padding: 50px 0px 20px 0px;
+  border-bottom:  black;
+}
+.my_employer{
+  color: rgb(255, 255, 255);
+  font-size: 50px;
+  padding-top: 20px;
+}
+.my_icon{
+  border-radius: 50%; 
+  margin: 50px 0px 0px 0px;
+  width: 300px;
+  height: 300px;
+}
+</style>
---
+++ src/components/Skill.vue
@@ -0,0 +1,47 @@
+
+<template>
+  <div>
+    <div class="Skill">
+      <h1 class="skill_set">Skill set</h1>
+      <div class="sentences_parent"> 
+        <p1 class="skill_sentences">自分の"好き"ができ、自信を持て、人に提供し、人を笑顔にできるクリエイターを目指し、好奇心の向くことにチャレンジを惜しまない。そのために現在は、自分が大学時代に興味を持ったIT技術を0から勉強しています。</p1>
+      </div>
+    </div>
+    <div>
+      <img class="my_icon" alt="Masayuki_Tagi" src="">
+    </div>
+  </div>
+</template>
+
+
+<!-- script -->
+
+<script>
+</script>
+
+
+<!-- CSS -->
+
+
+<style scoped>
+.Skill{
+  margin: 20px auto;
+  height: 170px;
+  width: 240px;
+}
+.skill_set{
+  text-align: center; 
+  white-space: pre-wrap;
+  font-size: 20px;
+  padding: 20px 0px 10px 0px;
+}
+.sentences_parent{
+  margin: 0 auto;
+  width: 200px;
+}
+.skill_sentences{
+  white-space: pre-wrap;
+  font-size: 7px;
+  padding-top: 10px;
+}
+</style>
